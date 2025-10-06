3521cc72f6294bce76d5124125228490adc49965
Terklnwza007 <tanawat11410@gmail.com>
2025-10-06 23:26:34 +0700

    get_db

--- app/database.py
+++ app/database.py
@@ -1,7 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres.kebhrgqohcqveendeksk:Teirk0894042414@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres"
+DATABASE_URL = "postgresql+psycopg2://postgres.kebhrgqohcqveendeksk:Teirk0894042414@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
