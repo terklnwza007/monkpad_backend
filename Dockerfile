bba18e5659d52388cd83dfcc946a653d52ca54fb
Terklnwza007 <tanawat11410@gmail.com>
2025-10-07 17:25:49 +0700

    cancel OCR to backend

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.11-slim
-RUN apt-get update && \
-    apt-get install -y tesseract-ocr tesseract-ocr-tha && \
-    apt-get clean && rm -rf /var/lib/apt/lists/*
-WORKDIR /app
-COPY . /app
-RUN pip install --no-cache-dir -r requirements.txt
-EXPOSE 10000
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "10000"]
--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import users, tags, month_results , transactions ,ocr
+from app.routers import users, tags, month_results , transactions 
 
 app = FastAPI()
 
@@ -8,4 +8,4 @@ app.include_router(users.router)
 app.include_router(tags.router)
 app.include_router(month_results.router)
 app.include_router(transactions.router)
-app.include_router(ocr.router)
+
